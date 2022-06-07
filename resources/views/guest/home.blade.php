36af7ade6e5065ba33c89575b946b1cc4cda3d26
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-06-07 20:35:41 +0200

    faker

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -81,20 +81,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{-- Pagina in costruzione --}}
                 </div>
+                <div id="root">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
+        {{-- Richiamare javascript con dentro vue --}}
+        <script src=" {{asset('js/front.js')}} "></script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('guest.home');
+// });
+
+Auth::routes();
+
+
+Route::middleware('auth')
+->prefix('admin')
+->name('admin.')
+->namespace('Admin')
+->group(function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
 });
+
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where("any", ".*");
